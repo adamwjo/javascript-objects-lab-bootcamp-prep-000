e82a92abf510623c22cff22ce6bae9b9189cb186
Adam Johnson <adamwjo@gmail.com>
2018-07-07 21:14:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ var recipes = new Object({})
 function updateObjectWithKeyAndValue(object, key value){
   return Object.assign({}, obj, { [key]: value })
 }
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  
+}
