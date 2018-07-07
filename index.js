21d428b68731cafbaa29e2a8139b3d984f11080c
Adam Johnson <adamwjo@gmail.com>
2018-07-07 21:01:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,15 +1,3 @@
-var recipes = new Object();
-
-function updateObjectWithKeyAndValue(object, key, value){
-  return Object.assign({}, object, { [key]: value })
-}
-
-function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
-  object[key] = value
-  return object
-}
-
-function destructivelyDeleteFromObjectByKey(object, key){
-  delete [object].key
-  return object
+function deleteFromObjectByKey(object, key){
+  
 }
