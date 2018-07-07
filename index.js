04f7ea0fd70cadb3c339bca967bb3504b99360be
Adam Johnson <adamwjo@gmail.com>
2018-07-07 21:22:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,5 +10,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
 }
 
 function deleteFromObjectByKey(object, key){
-  
+  object.assign({}, object)
+  delete. object.key
+  return object
 }
